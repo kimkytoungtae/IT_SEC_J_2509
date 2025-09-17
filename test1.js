1a79651b01f0ffeab2559300ed3ca7e77cec1655
kimkytoungtae <kind4209@gmail.com>
2025-09-17 10:47:27 +0900

    .

--- test1.js
+++ test1.js
@@ -14,4 +14,27 @@ a=1;
 // ++ -- 요놈은 특수연산
 // = 대입 연산자가 없어도, 값을 바꿔요
 a++;
-console.log(`a: ${a}`);
+//console.log(`a: ${a}`);
+
+
+/* 여러줄 주석
+상수 : 한번 정하면 절대 바뀌지 않는 변수
+*/
+const con1 = 3;
+
+a = 1;
+b = "2";
+c = a + b;
+//console.log(`c: ${c}`);
+
+/*
+num1 이라는 변수를 선언하고 3 이라는 값으로 초기화 해주세요
+num2 라는 변수를 선언하고 2 라는 값으로 초기화 해주세요
+result 라는 변수를 선언하고, num1/num2 값으로 초기화 해주세요
+*/
+let num1 = 3;
+console.log(`num1: ${num1} `);
+let num2 = 2;
+console.log(`num2: ${num2} `);
+let result = num1/num2;
+console.log(`num1/num2: ${num1/num2} `);
