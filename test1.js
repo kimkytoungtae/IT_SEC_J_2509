a20a7cb4a26a87a93069603961c46682e00ed7c0
kimkytoungtae <kind4209@gmail.com>
2025-09-17 10:30:13 +0900

    .

--- test1.js
+++ test1.js
@@ -5,6 +5,13 @@ let b = 1;
 let c = a + b;
 
 a + 3;
-console.log(`a:${a}`); // 1
+//console.log(`a:${a}`); // 1
 a = a + 3;
-console.log(`a:${a}`); // 4
+//console.log(`a:${a}`); // 4
+
+a=1;
+
+// ++ -- 요놈은 특수연산
+// = 대입 연산자가 없어도, 값을 바꿔요
+a++;
+console.log(`a: ${a}`);
