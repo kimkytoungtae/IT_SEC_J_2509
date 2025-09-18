caaca8d915dd2763a11d996ef8c533ac90b3ec2a
kimkytoungtae <kind4209@gmail.com>
2025-09-18 11:53:31 +0900

    ..

--- Day04_if.ts
+++ Day04_if.ts
@@ -45,6 +45,12 @@ if(score >=90){
 else if(score >=80 && score <=89){
     console.log("B");
 }
-else(score >=70 && score <=79){
+else if(score >=70 && score <=79){
     console.log("C");
+}
+else if(score >=60 && score <=69){
+    console.log("D");
+}
+else{
+    console.log("F");
 }
