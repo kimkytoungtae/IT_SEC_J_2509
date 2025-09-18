be47958b5e5b054e983a5f384e2e5b05386039d5
kimkytoungtae <kind4209@gmail.com>
2025-09-18 11:34:43 +0900

    ..

--- Day04_if.ts
+++ Day04_if.ts
@@ -16,8 +16,11 @@ if (user_input != door_password) {
 
 
 //if 문을 쉬운 형태로
-if (true) {
+if (false) {
     console.log("조건이 true 일때만 여기가 실행")
+}
+else if (true) {
+    console.log("if 가 조건이 안맞아서 else if가 실행 됬어요")
 } else {
-    console.log("조건이 false 일때 실행. 저는 옵션이에요")
+    console.log("if도, elseif 도 조건이 안맞아서 else가 실행됬어요")
 }
