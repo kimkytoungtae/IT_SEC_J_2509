31141d199feec7cbc8901e79a047234b995a704b
kimkytoungtae <kind4209@gmail.com>
2025-09-18 11:32:28 +0900

    ..

--- Day04_if.ts
+++ Day04_if.ts
@@ -1,9 +1,17 @@
 let user_input = 1234;
 let door_password = 1234;
+/*
 if (user_input == door_password) {
     console.log(`문을 열겠습니다`);
 } else {
     console.log(`비밀번호가 틀렸습니다`);
+}
+*/
+if (user_input == door_password) {
+    console.log(`비밀번호 ok`);
+}
+if (user_input != door_password) {
+    console.log(`비밀번호 틀림`);
 }
 
 
